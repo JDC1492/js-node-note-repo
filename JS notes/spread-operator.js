424e65fc6397d8cdbd88b257356bd8744868f735
JDC1492 <Jonathan_Cohen10@yahoo.com>
2022-05-11 19:51:19 -0400

    Note taking for the day complete

--- JS notes/spread-operator.js
+++ JS notes/spread-operator.js
@@ -1,7 +1,20 @@
 //May 11th, 2022
 
 //The Spread Operator
-//it can be thought of as a way to "dump" whats contained in the iterable out into a function.
+//For me, it can be thought of as a way to "dump" whats contained in the iterable(arr, sets, maps, strings) out into a function, or even another iterable.
 
 const arr = [7, 8, 9];
-console.log(arr);
+//^^ the orginal array
+const makeANewArr = [...arr, 10, 11, 12];
+//^^ creating a new array that will hold the individual elements of the ['arr' array] and added integers of 10, 11, and 12.
+console.log(arr, makeANewArr);
+
+//can also be used in functions as well.
+
+const names = new Array("Amy", "Tails", "Sonic");
+
+function allOfThem(name1, name2, name3) {
+  return `${name1},${name2}, and ${name3}..it's time to come home!`;
+}
+
+allOfThem(...names);
