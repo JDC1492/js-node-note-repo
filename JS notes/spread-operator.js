3b7b3a585c7e3210d15a0dde72793492dcf460ff
JDC1492 <Jonathan_Cohen10@yahoo.com>
2022-05-11 18:52:38 -0400

    note taking, installed node to run scripts and to help with node notes as well

---
+++ JS notes/spread-operator.js
@@ -0,0 +1,7 @@
+//May 11th, 2022
+
+//The Spread Operator
+//it can be thought of as a way to "dump" whats contained in the iterable out into a function.
+
+const arr = [7, 8, 9];
+console.log(arr);
